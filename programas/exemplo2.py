d9c1533554347e8ebd2cdf3bffeddd0cf0ddded3
oniyudi <onimaru.yudi@gmail.com>
2026-05-14 13:56:58 -0300

    editando grafico de barras

--- programas/exemplo2.py
+++ programas/exemplo2.py
@@ -13,15 +13,17 @@ df = df[df['svid'].isin(range(1, 37))]
 df_single = df[df['svid'].isin([19, 6])]
 df_single = df_single[df_single['time_utc'].between('2025-03-21 00:00:00', '2025-03-21 00:50:00')]
 
-df_bar = df[df['svid'].isin(range(1,10))]
-df_bar = df_bar[df_bar['time_utc'].between('2025-03-21 00:00:00', '2025-03-21 01:00:00')]
+df_bar = df[df['svid'].isin(range(1,20))]
+# df_bar = df_bar[df_bar['time_utc'].between('2025-03-21 00:00:00', '2025-03-21 01:00:00')]
+df_bar = df_bar[df_bar['time_utc'] == '2025-03-21 00:35:00']
+df_bar['svid'] = df_bar['svid'].astype(str)
 
 fig = px.line(df, x='time_utc', y='s4', color='svid', title='Índice S4 ao longo do tempo para cada satélite')
 
 fig_single = px.line(df_single, x='time_utc', y='s4', color='svid', text='elev', title='Índice S4 dos satélites 6 e 19 entre 00:00 e 00:50 do dia 21 de março de 2025')
 fig_single.update_traces(textposition='bottom right')
 
-fig_bar = px.bar(df_bar, x='time_utc', y='s4', color='s4', text='svid', title='Índice S4 ao longo do tempo para os satélites 1 a 9')
+fig_bar = px.bar(df_bar, x='svid', y='s4', color='s4', text='s4', title='Índice S4 dos satélites 1 a 19 às 00:01 do dia 21 de março de 2025')
 
 app = Dash(__name__)
 app.layout = html.Div(children=[
