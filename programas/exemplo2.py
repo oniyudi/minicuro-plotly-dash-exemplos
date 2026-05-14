a12119d4bc301d8fe5ca69296ef81aee6f380131
oniyudi <onimaru.yudi@gmail.com>
2026-05-13 23:52:10 -0300

    add grafico de barras

--- programas/exemplo2.py
+++ programas/exemplo2.py
@@ -13,10 +13,15 @@ df = df[df['svid'].isin(range(1, 37))]
 df_single = df[df['svid'].isin([19, 6])]
 df_single = df_single[df_single['time_utc'].between('2025-03-21 00:00:00', '2025-03-21 00:50:00')]
 
+df_bar = df[df['svid'].isin(range(1,10))]
+df_bar = df_bar[df_bar['time_utc'].between('2025-03-21 00:00:00', '2025-03-21 01:00:00')]
+
 fig = px.line(df, x='time_utc', y='s4', color='svid', title='Índice S4 ao longo do tempo para cada satélite')
 
 fig_single = px.line(df_single, x='time_utc', y='s4', color='svid', text='elev', title='Índice S4 dos satélites 6 e 19 entre 00:00 e 00:50 do dia 21 de março de 2025')
 fig_single.update_traces(textposition='bottom right')
+
+fig_bar = px.bar(df_bar, x='time_utc', y='s4', color='s4', text='svid', title='Índice S4 ao longo do tempo para os satélites 1 a 9')
 
 app = Dash(__name__)
 app.layout = html.Div(children=[
@@ -29,6 +34,10 @@ app.layout = html.Div(children=[
     dcc.Graph(
         id='s4-line-graph-single',
         figure=fig_single
+    ),
+    dcc.Graph(
+        id='s4-bar-graph',
+        figure=fig_bar
     )
 ])
 if __name__ == '__main__':
